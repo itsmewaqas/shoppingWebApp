451235e78b29e6ecd1a0ae7cac6ffa9675b1a8eb
itsmewaqas <mwdexter2@gmail.com>
2023-06-06 15:06:12 +0500

    reservation fixed
    reservation fixed

--- src/components/Reservations.jsx
+++ src/components/Reservations.jsx
@@ -134,7 +134,6 @@ function Reservations(props) {
     SetchooseMemberErr("");
     SetchooseTimeErr("");
     SetchooseOccasionErr("");
-
     SetfirstNameErr("");
     SetlastNameErr("");
     SetemailErr("");
@@ -147,7 +146,6 @@ function Reservations(props) {
 
   const step1 = () => {
     clearSubmission();
-
     if (seating == undefined || seating == "") {
       SetseatingErr("Select Seating");
     }
@@ -168,88 +166,76 @@ function Reservations(props) {
       chooseMember != '' &&
       chooseTime != '' &&
       chooseOccasion != '') {
-      // seating,
-      // reservedDate,
-      // chooseMember,
-      // chooseTime,
-      // chooseOccasion,
       SetformStep1(false);
       SetformStep2(true);
     }
     else {
       return false
     }
+  }
 
+  const editInfo = () => {
+    SetformStep1(true);
+    SetformStep2(false);
   }
 
   const reservedFunc = () => {
-    // clearSubmission();
-    // if (name == undefined || name == "" || name.length <= 5) {
-    //   SetnameErr("enter name above 5 character");
-    // }
-    // var emailRgx = /^([a-zA-Z0-9_\.\-])+\@(([a-zA-Z0-9\-])+\.)+([a-zA-Z0-9]{2,4})+$/;
-    // if (email == undefined || email == "" || !emailRgx.test(email)) {
-    //   SetemailErr("enter your valid email address");
-    // }
-    // var cellRgx = /^[\+]?[(]?[0-9]{3}[)]?[-\s\.]?[0-9]{3}[-\s\.]?[0-9]{4,6}$/im;
-    // if (cell == undefined || cell == "" || (cellRgx.test(cell) == false)) {
-    //   SetcellErr("enter your valid cell number");
-    // }
-    // if (ofGuests == undefined || ofGuests == "") {
-    //   SetofGuestsErr("please enter data");
-    // }
-    // if (reservationDate == undefined || reservationDate == "") {
-    //   SetreservationDateErr("select reservation date");
-    // }
-    // if (reservationTime == undefined || reservationTime == "") {
-    //   SetreservationTimeErr("select reservation time");
-    // }
-    // if (reservationType == undefined || reservationType == "") {
-    //   SetreservationTypeErr("select reservation time");
-    // }
-    // if (ifOther == undefined || ifOther == "") {
-    //   SetifOtherErr("please enter if other");
-    // }
-    // if (anySpecial == undefined || anySpecial == "") {
-    //   SetanySpecialErr("please enter any special requests");
-    // }
-    // if (name != '' &&
-    //   email != '' &&
-    //   cell != '' &&
-    //   ofGuests != '' &&
-    //   reservationDate != '' &&
-    //   reservationTime != '' &&
-    //   reservationType != '' &&
-    //   ifOther != '' &&
-    //   anySpecial != '') {
-    //   const getdata = {
-    //     name,
-    //     email,
-    //     cell,
-    //     ofGuests,
-    //     reservationDate,
-    //     reservationTime,
-    //     reservationType,
-    //     ifOther,
-    //     anySpecial,
-    //   }
-    //   Setfinaldata(getdata);
-    //   setTimeout(() => {
-    //     Setname('');
-    //     Setemail('');
-    //     Setcell('');
-    //     SetofGuests('');
-    //     SetreservationDate('');
-    //     SetreservationTime('');
-    //     SetreservationType('');
-    //     SetifOther('');
-    //     SetanySpecial('');
-    //     SetsuccessBox(true);
-    //   }, 1000);
-    // }
-    // else {
-    //   return false
-    // }
+    clearSubmission();
+    if (firstName == undefined || firstName == "" || firstName.length <= 5) {
+      SetfirstNameErr("enter first name above 5 character");
+    }
+    if (lastName == undefined || lastName == "" || lastName.length <= 5) {
+      SetlastNameErr("enter last name above 5 character");
+    }
+    var emailRgx = /^([a-zA-Z0-9_\.\-])+\@(([a-zA-Z0-9\-])+\.)+([a-zA-Z0-9]{2,4})+$/;
+    if (email == undefined || email == "" || !emailRgx.test(email)) {
+      SetemailErr("enter your valid email address");
+    }
+    var cellRgx = /^[\+]?[(]?[0-9]{3}[)]?[-\s\.]?[0-9]{3}[-\s\.]?[0-9]{4,6}$/im;
+    if (cell == undefined || cell == "" || (cellRgx.test(cell) == false)) {
+      SetcellErr("enter your valid cell number");
+    }
+    if (specialRequest == undefined || specialRequest == "") {
+      SetspecialRequestErr("you can write here special request");
+    }
+    if (firstName != '' &&
+      lastName != '' &&
+      email != '' &&
+      cell != '' &&
+      specialRequest != '') {
+      const getdata = {
+        seating,
+        reservedDate,
+        chooseMember,
+        chooseTime,
+        chooseOccasion,
+        firstName,
+        lastName,
+        email,
+        cell,
+        specialRequest,
+      }
+      Setfinaldata(getdata);
+      setTimeout(() => {
+        Setseating('');
+        SetreservedDate('');
+        SetchooseMember('');
+        SetchooseTime('');
+        SetchooseOccasion('');
+        SetfirstName('');
+        SetlastName('');
+        Setemail('');
+        Setcell('');
+        SetspecialRequest('');
+        SetformStep1(true);
+        SetformStep2(false);
+        SetsuccessBox(true);
+      }, 1000);
+      console.log(getdata);
+    }
+    else {
+      return false
+    }
   }
 
   useEffect(() => {
@@ -264,18 +250,13 @@ function Reservations(props) {
             <button className='close' onClick={() => SetsuccessBox(false)}>X</button>
             <p>
               <strong>All good!</strong>
-              Thanks you for the Restaurant Reservation '{finaldata.name}' we will meet in the restaurant with best services.
+              Thanks you for the Restaurant Reservation '{finaldata.firstName}' we will meet in the restaurant with best services.
             </p>
           </div>
         </dd>}
-
-
-
-
       <div className='reservationsBlock'>
-        <div className='container clearfix' style={{ paddingBottom: '180px' }}>
+        <div className='container clearfix'>
           <h3>Reservation</h3>
-
           {formStep1 &&
             <div className='reservationsFormBlock'>
               <div>
@@ -331,8 +312,6 @@ function Reservations(props) {
                 <button className='reserveBtn' onClick={() => step1()}>Next</button>
               </div>
             </div>}
-
-
           {formStep2 &&
             <div className='reservationsFormBlock'>
               <div className='reservationsForm'>
@@ -357,6 +336,8 @@ function Reservations(props) {
               </div>
               <div className='fetchInfo'>
                 <p><span>{seating}</span></p>
+                <p><button className='editInfoBtn' onClick={() => editInfo()}><img src={require('../assets/img/editInfo.png')} /></button></p>
+                <div className='clearfix'></div>
                 <p><img src={require('../assets/img/peopleIcon1.png')} /><span>{chooseMember}</span></p>
                 <p><img src={require('../assets/img/timeIcon1.png')} /><span>{chooseTime}</span></p>
                 <p><img src={require('../assets/img/occasionIcon1.png')} /><span>{chooseOccasion}</span></p>
@@ -372,120 +353,7 @@ function Reservations(props) {
                 <button className='reserveBtn' onClick={() => reservedFunc()}>Reserved</button>
               </div>
             </div>}
-
-
-
-
-
-
-
-
-
-
-
-
-
-
         </div>
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-        {/* <div className='container'>
-          <div className='formBox' style={{ marginTop: '200px' }}>
-            <div>
-              <label>Full Name</label>
-              <input type="text" value={name} onChange={(e) => Setname(e.target.value)} />
-              <p className='error'>{nameErr}</p>
-            </div>
-            <div>
-              <label>Email</label>
-              <input type="email" value={email} onChange={(e) => Setemail(e.target.value)} />
-              <p className='error'>{emailErr}</p>
-            </div>
-            <div>
-              <label>Cell</label>
-              <input type="text" value={cell} onChange={(e) => Setcell(e.target.value)} />
-              <p className='error'>{cellErr}</p>
-            </div>
-            <div>
-              <label>of Guests</label>
-              <input type="text" value={ofGuests} onChange={(e) => SetofGuests(e.target.value)} />
-              <p className='error'>{ofGuestsErr}</p>
-            </div>
-            <div>
-              <label>Select Reservation Date</label>
-              <input type="date" value={reservationDate} onChange={(e) => SetreservationDate(e.target.value)} />
-              <p className='error'>{reservationDateErr}</p>
-            </div>
-            <div>
-              <label>Select Time</label>
-              <input type="time" value={reservationTime} onChange={(e) => SetreservationTime(e.target.value)} />
-              <p className='error'>{reservationTimeErr}</p>
-            </div>
-            <div>
-              <label>Reservation Type</label>
-              <select value={reservationType} onChange={(e) => { SetreservationType(e.target.value) }}>
-                <option value="">Please Select</option>
-                <option value="Dinner">Dinner</option>
-                <option value="VIP/Mezzanine">VIP/Mezzanine</option>
-                <option value="Birthday/ Anniversary">Birthday/ Anniversary</option>
-                <option value="Nightlife">Nightlife</option>
-                <option value="Private">Private</option>
-                <option value="Wedding">Wedding</option>
-                <option value="Corporate">Corporate</option>
-                <option value="Holiday">Holiday</option>
-                <option value="Other">Other</option>
-              </select>
-              <p className='error'>{reservationTypeErr}</p>
-            </div>
-            <div>
-              <label>If Other above, please specify</label>
-              <input type="text" value={ifOther} onChange={(e) => SetifOther(e.target.value)} />
-              <p className='error'>{ifOtherErr}</p>
-            </div>
-            <div>
-              <label>Any special requests</label>
-              <textarea value={anySpecial} onChange={(e) => SetanySpecial(e.target.value)}></textarea>
-              <p className='error'>{anySpecialErr}</p>
-            </div>
-          </div>
-          <button className='reserveBtn' onClick={() => reservedFunc()}>Reserve</button>
-        </div> */}
-
-
-
-
       </div>
     </div>
   );
